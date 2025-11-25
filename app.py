9667f64b61795b97b4fc80119e96f120ed50fb4f
TejasPund2002 <tejas1234rk@gmail.com>
2025-11-25 17:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,31 @@ from google.genai import Client
 
 API_KEY = st.secrets["GOOGLE_API_KEY"]
 
-client = genai.Client(api_key=API_KEY)
+client = Client(api_key=API_KEY)
 
 st.title("My AI Agent")
 
+# Session for chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# User input
 user_input = st.chat_input("Ask something...")
 
 if user_input:
     st.session_state.messages.append({"role": "user", "content": user_input})
-    response = client.models.generate_content(
+
+    # Generate AI response
+    response = client.responses.generate(
         model="gemini-2.0-flash",
-        contents=user_input, 
-        )
+        input=user_input
+    )
+
     ai_msg = response.output_text
+
     st.session_state.messages.append({"role": "assistant", "content": ai_msg})
 
+# Display chat history
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.write(msg["content"])
-
-
-
-
-
